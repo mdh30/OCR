9e3f78226c027ad7f54db6ceacf95b060789196f
Nishant Rodrigues <nishnash54@gmail.com>
2018-10-14 01:33:54 +0530

    Server setup

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
-# from process_image import process_img
+from process_image import process_img
 from extract_text import img_text
 
+import re
 import json
+import base64
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/ocr')
 def OCR():
-    # process_img()
-    a = img_text()
-    a = json.dumps({'movie': a})
-    return a
+    
+    img_data = request.args.get('img')[22:]
+    img_data = re.sub(' ', '+', img_data)
+    img_data = bytes(img_data, 'utf=8')
+
+    print(img_data[:100])
+    
+    print(type(img_data))
+
+    with open("raw_file.png", "wb") as f:
+        f.write(base64.decodebytes(img_data))
+
+    process_img("raw_file.png", "processed_file.png")
+    response = json.dumps({'ocrText': img_text()})
+    return response
 
 if __name__ == '__main__':
     app.run()
