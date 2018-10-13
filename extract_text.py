c673c0e31f1c9dc27fb0181838ee0a7f82cbdc2e
Nishant Rodrigues <nishnash54@gmail.com>
2018-10-14 01:32:18 +0530

    Set file names

--- extract_text.py
+++ extract_text.py
@@ -2,13 +2,16 @@ from PIL import Image
 import pytesseract
 import time
 import urllib
-start_time = time.time()
-start_time2 = time.time()
-print("serial execution")
-print("--- %s seconds prepair---" % (time.time() - start_time))
-start_time = time.time()
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
-a = pytesseract.image_to_string(Image.open('text.jpg'))
-print(a)
-print("--- %s seconds read---" % (time.time() - start_time))
-#print("--- %s seconds read---" % (time.time() - start_time2))
+
+# Main function to call
+def img_text():
+    
+    start_time = time.time()
+    print("--- %s seconds prepair---" % (time.time() - start_time))
+    start_time = time.time()
+    pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+    text = pytesseract.image_to_string(Image.open("processed_file.png"))
+    print(text)
+    print("--- %s seconds read---" % (time.time() - start_time))
+    
+    return text
